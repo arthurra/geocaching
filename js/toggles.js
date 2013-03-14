2a9e0b2a9de08631364978816030cb0ae82902e8
Ryan Arthur <ryan.arthur5819@gmail.com>
2013-03-14 11:27:41 -0400

    menu toggle - geolocation map integration

---
+++ js/toggles.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+ 
+  $('div.new-post-btn').click(function() {
+    $('div.compass-container').toggleClass('open');
+  });
+
+  $('div.back-btn').click(function() {
+    $('div.compass-container').toggleClass('open');
+  });
+  
+});
